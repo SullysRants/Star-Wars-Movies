2be8a5102c040748bd95f23f4ec5fd6909cef12a
Kevin Zavier <kzavier@ucsd.edu>
2016-09-13 11:12:54 -0700

    added menu

--- app.js
+++ app.js
@@ -3,29 +3,21 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+var routes = require('./routes');
 
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 
 //home
-app.get('/', function(req, res){
-    res.render('home', {
-        title : "Star Wars Movies",
-        movies : ["The First Move", "The Second Movie", "The Third Movie"]
-    });
-});
+app.get('/', routes.home);
 
 //move_single
-app.get('/star_wars_episode/:episode_number?', function(req, res){
-    var episode_number = req.params.episode_number;
-    res.send("This is the page for episode " + episode_number);
-});
+app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 
 //notFound
-app.get('*', function(req,res){
-    res.send("This is not the page that you are looking for");
-});
-
+app.get('*', routes.notFound);
 
 app.listen(3000, function(){
     console.log("The application is running on localhost:3000");
