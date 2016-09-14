cd14418027012c944b69a864e9ec5fcda0c93061
Kevin Zavier <kzavier@ucsd.edu>
2016-09-13 17:06:55 -0700

    Got the links to show when user clicks poster

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ exports.home = function(req, res) {
 
 	var movies = moviesJSON.movies
 
-   	res.render('home', {
+	res.render('home', {
 		title : "Star Wars Movies",
 		movies : movies
 	});
@@ -17,19 +17,30 @@ exports.movie_single =  function(req, res){
 
 	var movies = moviesJSON.movies
 
-    var episode_number = req.params.episode_number;
+	var episode_number = req.params.episode_number;
 
-    var movie = movies[episode_number - 1];
+	if(episode_number >= 1 && episode_number <= 6){
 
-    var title = movie.title;
+		var movie = movies[episode_number - 1];
 
-    res.render('movie_single', {
-    	title : title,
-    	movies : movies
-    });
+		var title = movie.title;
+
+		var main_characters = movie.main_characters;
+
+		res.render('movie_single', {
+			title : title,
+			movies : movies,
+			movie : movie,
+			main_characters : main_characters
+		});
+	}
+	else{
+		res.send("This is not the page you are looking for");
+	}
+
 };
 
 //notFound
 exports.notFound = function(req,res){
-    res.send("This is not the page that you are looking for");
+	res.send("This is not the page that you are looking for");
 };
