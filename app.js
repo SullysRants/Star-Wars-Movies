1c559892281b204ed67f0bc68dff21d0f0f2f595
Kevin Zavier <kzavier@ucsd.edu>
2016-09-13 17:50:10 -0700

    Put up the footer

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ app.get('/star_wars_episode/:episode_number?', routes.movie_single);
 //notFound
 app.get('*', routes.notFound);
 
+/*
 app.listen(3000, function(){
     console.log("The application is running on localhost:3000");
 });
+*/
 
+app.listen(process.env.PORT || 3000);
